cd0f3be3aa552150f506bfe4bf2f6cb72df83f5c
Martina Skrypnyk <37904208+SchematicM@users.noreply.github.com>
2020-03-22 17:39:37 +0300

    Created view of table with additional subjects for students(Name,teacher,URL,numberOFstudents)

--- CourseSelect/Controllers/HomeController.cs
+++ CourseSelect/Controllers/HomeController.cs
@@ -1,16 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 using System.Web;
 using System.Web.Mvc;
+using CourseSelect.Models;
 
 namespace CourseSelect.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private List<Subjects> emp;
+        public HomeController()
+        {
+
+            emp = new List<Subjects>()
+            {
+
+                new Subjects()
+                {nameOfcourse="Криптологія",teacherName="Малець", studentsAmount=300, desc_href="https://ami.lnu.edu.ua/" },
+                new Subjects()
+                { nameOfcourse="Орфографія",teacherName="Королль", studentsAmount=500, desc_href="https://ami.lnu.edu.ua/" },
+                new Subjects()
+                { nameOfcourse="Батьківство",teacherName="Kальченко", studentsAmount=100, desc_href="https://ami.lnu.edu.ua/"}
+            };
+        }
+            public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult SubjectsData()
+        {
+            return Json(emp, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
---
+++ CourseSelect/Models/Subjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseSelect.Models
+{
+    public class Subjects
+    {
+        public string nameOfcourse { get; set; }
+
+        public string teacherName { get; set; }
+
+        public int studentsAmount { get; set; }
+        public string desc_href { get; set; }
+
+      
+
+    }
+}
