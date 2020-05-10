b538c1e6cb1117fa54228b8ecf55c26ba5f44446
SchematicM <martinez@gmail.com>
2020-05-10 12:23:33 +0300

    Created and added db project

---
+++ Model/CSelectContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Model.Tables;
+
+namespace Model
+{
+    public partial class CSelectContext : DbContext
+    {
+        public CSelectContext()
+        {
+        }
+
+        public CSelectContext(DbContextOptions<CSelectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Dbbc> Dbbc { get; set; }
+        public virtual DbSet<Table> Table { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CSelect;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dbbc>(entity =>
+            {
+                entity.ToTable("DBBC");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Description).IsUnicode(false);
+
+                entity.Property(e => e.EndDate).HasColumnType("date");
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+
+                entity.HasOne(d => d.Teacher)
+                    .WithMany(p => p.Dbbc)
+                    .HasForeignKey(d => d.TeacherId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_DBBC_ToUser");
+            });
+
+            modelBuilder.Entity<Table>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Dbbcid).HasColumnName("DBBCId");
+
+                entity.HasOne(d => d.Dbbc)
+                    .WithMany(p => p.Table)
+                    .HasForeignKey(d => d.Dbbcid)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Table_ToDBBC");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Table)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Table_ToUser");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Credit).IsUnicode(false);
+
+                entity.Property(e => e.FatherName).IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Surname).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
