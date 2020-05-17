14cfa5577ab56e7bc58a4887d0354e4dc5e2e266
SchematicM <tyxa.khoma16@gmail.com>
2020-05-17 16:30:05 +0300

    Added users service

---
+++ Infrastructure/Services/Implementations/UsersService.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Services.Interfaces;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Services.Implementations
+{
+    public class UsersService : IUsersService
+    {
+        private CoursesContext _context;
+
+        public UsersService(CoursesContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<AspNetUsers> GetUsers()
+        {
+            var users = _context.AspNetUsers.AsEnumerable();
+
+            return users;
+        }
+
+        public IEnumerable<AspNetUsers> GetUserByCredit(string credit)
+        {
+            var user = _context.AspNetUsers.Where(user => user.Credit == credit);
+
+            return user;
+        }
+    }
+}
