3918d9764c7e068c9694f5bf9b94dd988778410e
VladChemerskyi <prostoklass@gmail.com>
2020-05-17 15:25:25 +0300

    Added database connection

---
+++ Infrastructure/Services/Implmentations/DBBCService.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Services.Interfaces;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Services.Implmentations
+{
+    public class DBBCService: IDBBCService
+    {
+        private CoursesContext _context;
+
+        public DBBCService(CoursesContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Dbbc> GetDBBC()
+        {
+            var dbbcs = _context.Dbbc.AsEnumerable();
+
+            return dbbcs;
+        }
+    }
+}
---
+++ Infrastructure/Services/Interfaces/IUsersService.cs
@@ -0,0 +1,13 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface IUsersService
+    {
+        public IEnumerable<AspNetUsers> GetUsers();
+        public IEnumerable<AspNetUsers> GetUserByCredit(string credit);
+    }
+}
