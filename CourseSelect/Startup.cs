3ac2d596203134ff4b554bd2bc8313992d482f90
SolomiyaRybai <Solomiyar2000@gmail.com>
2020-05-17 19:14:47 +0300

    Added swagger

--- CourseSelect/Startup.cs
+++ CourseSelect/Startup.cs
@@ -15,6 +15,8 @@ using Infrastructure;
 using Model;
 using Infrastructure.Services.Interfaces;
 using Infrastructure.Services.Implementations;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace CourseSelect
 {
@@ -54,6 +56,11 @@ namespace CourseSelect
             services.AddControllersWithViews();
 
             services.AddRazorPages();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,6 +77,7 @@ namespace CourseSelect
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSwagger();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -84,6 +92,11 @@ namespace CourseSelect
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
--- Infrastructure/Services/Interfaces/IDBBCToUserService.cs
+++ Infrastructure/Services/Interfaces/IDBBCToUserService.cs
@@ -10,6 +10,6 @@ namespace Infrastructure.Services.Interfaces
         public IEnumerable<Dbbctouser> GetUsersDbbc();
         public IEnumerable<Dbbctouser> GetUsersDbbcByUserId(string userId);
         public bool AddDBBCToUser(Dbbctouser dbbcToUser);
-        public bool Save()
+        public bool Save();
     }
 }
