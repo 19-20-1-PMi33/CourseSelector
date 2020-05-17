a7d414ea6698006952d5a9b637170aa73a002e6d
SchematicM <tyxa.khoma16@gmail.com>
2020-05-17 16:30:17 +0300

    Added Dbbc to user service

---
+++ Infrastructure/Services/Interfaces/IDBBCToUserService.cs
@@ -0,0 +1,13 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface IDBBCToUserService
+    {
+        public IEnumerable<Dbbctouser> GetUsersDbbc();
+        public IEnumerable<Dbbctouser> GetUsersDbbcByUserId(string userId);
+    }
+}
