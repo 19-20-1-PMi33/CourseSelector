38f369ba0d219bddf698788a032e0f67a0984871
SchematicM <tyxa.khoma16@gmail.com>
2020-05-18 01:55:47 +0300

    Fixed user finding

--- CourseSelect/Controllers/CourseController.cs
+++ CourseSelect/Controllers/CourseController.cs
@@ -8,15 +8,19 @@ using Microsoft.AspNetCore.Mvc;
 using Model;
 
 namespace CourseSelect.Controllers
-{   
+{
     [Authorize]
     public class CourseController : Controller
     {
         private readonly IDBBCService _dBBCService;
+        private readonly IUsersService _usersService;
 
-        public CourseController(IDBBCService dBBCService)
+        public CourseController(
+            IDBBCService dBBCService,
+            IUsersService usersService)
         {
             _dBBCService = dBBCService;
+            _usersService = usersService;
         }
 
         public IActionResult CourseList()
@@ -25,8 +29,20 @@ namespace CourseSelect.Controllers
             return View(dbbcs);
         }
 
-        public IActionResult Course(Dbbc model)
+
+        public IActionResult Course(string description, string endDate, string startDate, string numberOfSeats, string numberOfSeatsUsed, string teacherId)
         {
+            var teacher = _usersService.getUserById(Convert.ToInt32(teacherId));
+
+            var model = new Dbbc()
+            {
+                Description = description,
+                StartDate = DateTime.Parse(startDate),
+                EndDate = DateTime.Parse(endDate),
+                NumberOfSeats = Convert.ToInt32(numberOfSeats),
+                NumberSetsUsed = Convert.ToInt32(numberOfSeatsUsed)
+            };
+
             return View(model);
         }
     }
