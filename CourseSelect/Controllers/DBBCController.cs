be5a51d4717a661a781e7b051b76db3641a7fc09
DanyloTymets <danylotymets@gmail.com>
2020-05-18 03:25:05 +0300

    Added dbbc controller

---
+++ CourseSelect/Controllers/DBBCController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Security.Claims;
+
+namespace CourseSelect.Controllers
+{
+    public class DBBCController : Controller
+    {
+        private readonly IDBBCToUserService _dBBCToUserService;
+        private readonly 
+
+        public DBBCController(
+            IDBBCToUserService dBBCToUserService,
+            IDBBCService dBBCService)
+        {
+            _dBBCToUserService = dBBCToUserService;
+        }
+
+        public IActionResult Subscribe(int dbbcId)
+        {
+            Dbbctouser dbbctouser = new Dbbctouser()
+            {
+                Dbbcid = dbbcId,
+                UserId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            };
+
+            _dBBCToUserService.AddDBBCToUser(dbbctouser);
+            _dBBCToUserService.Save();
+
+            return View();
+        }
+    }
+}
