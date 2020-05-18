2faa48dfe69b23c7b044d55092e3a18c2a0e44cd
Osttik <lololka23@gmail.com>
2020-05-18 03:08:36 +0300

    Toastr

--- CourseSelect/wwwroot/js/site.js
+++ CourseSelect/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+toastr.options.extendedTimeOut = 3000;
+toastr.options.timeOut = 3000;
+toastr.options.closeButton = true;
+toastr.options.positionClass = 'toast-bottom-right';
+
+function onSubscribe() {
+
+    toastr.info('Succeesfuly subscribed');
+}
