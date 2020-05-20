30ee6aa40f08b38c5faff97be78dff374936638c
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2020-05-20 16:52:34 +0300

    Added posibility to add admin

--- Model/AspNetUsers.cs
+++ Model/AspNetUsers.cs
@@ -13,6 +13,7 @@ namespace Model
         }
         public string Surname { get; set; }
         public string Credit { get; set; }
+        public bool? IsAdmin { get; set; }
 
         public virtual ICollection<Dbbc> Dbbc { get; set; }
         public virtual ICollection<Dbbctouser> Dbbctouser { get; set; }
