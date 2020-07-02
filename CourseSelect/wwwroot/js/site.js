c2fbde0f5f836ed931a0403deea1625edf65354b
VladChemerskyi <chemerskyivlad@gmail.com>
2020-07-02 15:37:25 +0300

    Task 1

--- CourseSelect/wwwroot/js/site.js
+++ CourseSelect/wwwroot/js/site.js
@@ -1,9 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-toastr.options.extendedTimeOut = 3000;
+﻿toastr.options.extendedTimeOut = 3000;
 toastr.options.timeOut = 3000;
 toastr.options.closeButton = true;
 toastr.options.positionClass = 'toast-bottom-right';
@@ -12,3 +7,17 @@ function onSubscribe() {
 
     toastr.info('Succeesfuly subscribed');
 }
+
+$(document).ready(function () {
+    $("#CourseListSearch").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+
+        $("#CourseList").children().each(function (a, b) {
+            var needToggle = false
+            $(b).find(".description-text").each(function (aa, bb) {
+                needToggle = bb.textContent.toLowerCase().indexOf(value) > -1 || needToggle
+            })
+            $(b).toggle(needToggle)
+        });
+    });
+});
